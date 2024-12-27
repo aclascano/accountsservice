7e1dbfe97c1d5851c5db9dd2523dd06ab965351a
aclascano <andreacarolinalascanovalencia@gmail.com>
2024-12-27 00:06:00 -0500

    proyecto conectado a bd

---
+++ src/test/java/com/aldev/accountservice/AccountsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldev.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
